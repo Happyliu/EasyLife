027616330f5bf01f97c7ca2229cdfda0930d9d3d
Zihao Zhang <zihao.h.zhang@gmail.com>
2014-06-19 20:55:10 +0800

    added the icon for divider, start the work of it with navigation bar

---
+++ EasyLife/DividerViewController.h
@@ -0,0 +1,13 @@
+//
+//  DividerViewController.h
+//  EasyLife
+//
+//  Created by 张 子豪 on 6/19/14.
+//  Copyright (c) 2014 Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DividerViewController : UIViewController
+
+@end
---
+++ EasyLife/DividerViewController.m
@@ -0,0 +1,71 @@
+//
+//  DividerViewController.m
+//  EasyLife
+//
+//  Created by 张 子豪 on 6/19/14.
+//  Copyright (c) 2014 Albert. All rights reserved.
+//
+
+#import "DividerViewController.h"
+#import "EasyLifeAppDelegate.h"
+
+@interface DividerViewController ()
+@property (strong, nonatomic) UIColor *appTintColor, *appSecondColor, *appThirdColor, *appBlackColor;
+
+@end
+
+@implementation DividerViewController
+
+#pragma mark - Initialize app colors
+
+- (UIColor *)appTintColor
+{
+    if (!_appTintColor) {
+        EasyLifeAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+        _appTintColor = appDelegate.appTintColor;
+    }
+    return _appTintColor;
+}
+
+- (UIColor *)appSecondColor
+{
+    if (!_appSecondColor) {
+        EasyLifeAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+        _appSecondColor = appDelegate.appSecondColor;
+    }
+    return _appSecondColor;
+}
+
+- (UIColor *)appThirdColor
+{
+    if (!_appThirdColor) {
+        EasyLifeAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+        _appThirdColor = appDelegate.appThirdColor;
+    }
+    return _appThirdColor;
+}
+
+- (UIColor *)appBlackColor
+{
+    if (!_appBlackColor) {
+        EasyLifeAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+        _appBlackColor = appDelegate.appBlackColor;
+    }
+    return _appBlackColor;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.navigationController.navigationBar.barTintColor = self.appTintColor;
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor]; // color of the back button
+    self.navigationController.navigationBar.translucent = NO;
+    
+    [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
+    self.tabBarController.tabBar.barTintColor = self.appTintColor;
+    self.tabBarController.tabBar.tintColor = [UIColor whiteColor];
+    self.tabBarController.tabBar.translucent = NO;
+}
+
+@end
